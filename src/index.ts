984e0595484586623b464f4c880bafa0906113cc
Alex <alex.podikoglou@gmail.com>
2026-02-07 14:01:35 +0200

    feat: queue

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Args, Command, Options } from "@effect/cli";
 import { FetchHttpClient } from "@effect/platform";
 import { BunContext, BunRuntime } from "@effect/platform-bun";
-import { Effect, Layer } from "effect";
+import { Effect, Layer, Queue } from "effect";
 import { Web } from "./web";
 
 const url = Args.text({
@@ -17,11 +17,14 @@ const linden = Command.make("linden", { url, depth }, ({ url, depth }) => {
 	return Effect.gen(function* () {
 		const web = yield* Web;
 
-		const links = yield* web
-			.fetchPage(new URL(url))
-			.pipe(Effect.flatMap(web.extractLinks));
+		const queue = yield* Queue.unbounded<{ url: URL; depth: number }>();
 
-		yield* Effect.log(links);
+		// initial seeding of the queue
+		yield* web.fetchPage(new URL(url)).pipe(
+			Effect.flatMap(web.extractURLs),
+			Effect.andThen((urls) => urls.map((url) => ({ url, depth: 0 }))),
+			Effect.andThen((items) => queue.offerAll(items)),
+		);
 
 		return yield* Effect.succeedNone;
 	});
